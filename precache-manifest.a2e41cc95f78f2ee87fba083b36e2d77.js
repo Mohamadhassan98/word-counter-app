856e222953dc95c404d4bc67c2484a193f840bc5
Mohamadhassan <emohamadhassan@gmail.com>
2019-10-27 02:22:22 +0330

    Updates

---
+++ precache-manifest.a2e41cc95f78f2ee87fba083b36e2d77.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16e6a1b09df51dbdad792a2af2a64b00",
+    "url": "/word-counter/index.html"
+  },
+  {
+    "revision": "5819f79a0e0efdf31cc0",
+    "url": "/word-counter/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "f91239fcafbfac15e83a",
+    "url": "/word-counter/static/js/2.a6d1271b.chunk.js"
+  },
+  {
+    "revision": "5819f79a0e0efdf31cc0",
+    "url": "/word-counter/static/js/main.e8ee95df.chunk.js"
+  },
+  {
+    "revision": "ded78b7bcdea9bf853c7",
+    "url": "/word-counter/static/js/runtime-main.46425581.js"
+  }
+]);
